55e209a9501a73ebeea8fa3e3ab119773a2ee270
Olivier Van hamme <nicefiction@gmail.com>
2021-04-19 11:19:56 +0200

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Olivier Van hamme on 19/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
